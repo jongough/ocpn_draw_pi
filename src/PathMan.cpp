26bec24421b195ea857b3cba60018981357dfe56
Jon Gough <jon.gough@eclipsesystems.com.au>
2015-03-13 19:16:41 +1100

    Fix header file name

--- src/PathMan.cpp
+++ src/PathMan.cpp
@@ -23,7 +23,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  **************************************************************************/
 
-#include "pathman.h"
+#include "PathMan.h"
 
 #include "wx/wxprec.h"
 
