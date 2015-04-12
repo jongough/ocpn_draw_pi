d41bd28bd8248b0b25b0ddcec211210e2d2c7614
Jon Gough <jon.gough@eclipsesystems.com.au>
2015-04-12 19:28:41 +1000

    Add new Properties page

---
+++ Forms/OCPNDrawPropertiesImpl.cpp
@@ -0,0 +1,23 @@
+#include "OCPNDrawPropertiesImpl.h"
+
+OCPNDrawPropertiesImpl::OCPNDrawPropertiesImpl( wxWindow* parent )
+:
+OCPNDrawPropertiesDialog( parent )
+{
+
+}
+
+void OCPNDrawPropertiesImpl::OnDrawPropertiesOKClick( wxCommandEvent& event )
+{
+	// TODO: Implement OnDrawPropertiesOKClick
+}
+
+void OCPNDrawPropertiesImpl::OnDrawPropertiesCancelClick( wxCommandEvent& event )
+{
+	// TODO: Implement OnDrawPropertiesCancelClick
+}
+
+void OCPNDrawPropertiesImpl::OnDrawPropertiesApplyClick( wxCommandEvent& event )
+{
+	// TODO: Implement OnDrawPropertiesApplyClick
+}
