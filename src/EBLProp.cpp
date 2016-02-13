635ab15235ee09297a930741f323a26219f1256f
Jon Gough <jon.gough@eclipsesystems.com.au>
2016-02-14 09:30:51 +1100

    Fix for windows compile

--- src/EBLProp.cpp
+++ src/EBLProp.cpp
@@ -32,9 +32,9 @@
 #include <wx/valnum.h>
 #endif
 
-extern EBLList                 *g_pEBLList;
-extern ocpn_draw_pi            *g_ocpn_draw_pi;
-extern PlugIn_Position_Fix_Ex   g_pfFix;
+extern EBLList                  *g_pEBLList;
+extern ocpn_draw_pi             *g_ocpn_draw_pi;
+extern ODPlugIn_Position_Fix_Ex g_pfFix;
 
 EBLProp::EBLProp()
 {
