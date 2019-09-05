e125bc16e4b3724abe96c86c40081c3edc44bc2f
jongough <jon.gough@eclipsesystems.com.au>
2019-09-05 13:46:31 +1000

    remove erroneous comment

--- ci/travis-build-osx.sh
+++ ci/travis-build-osx.sh
@@ -33,10 +33,10 @@ cmake -DOCPN_CI_BUILD=$CI_BUILD \
   -DCMAKE_INSTALL_PREFIX=/tmp/opencpn -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
   ..
 make -sj2
-mkdir -p /tmp/opencpn/bin/OpenCPN.app/Contents/MacOS
-mkdir -p /tmp/opencpn/bin/OpenCPN.app/Contents/SharedSupport/plugins
+mkdir -p /tmp/opencpn/bin/ocpn_draw_pi.app/Contents/MacOS
+mkdir -p /tmp/opencpn/bin/ocpn_draw_pi.app/Contents/SharedSupport/plugins
 chmod 644 /usr/local/lib/lib*.dylib
 make install
-make install # Dunno why the second is needed but it is, otherwise
+#make install # Dunno why the second is needed but it is, otherwise
              # plugin data is not included in the bundle
 make create-dmg
