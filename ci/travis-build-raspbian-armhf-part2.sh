8ff798be94f8e78c4f31fabfe3b802980d58f754
jongough <jon.gough@eclipsesystems.com.au>
2020-04-19 18:34:18 +1000

    Update to match current testplugin_pi versions of files

--- ci/travis-build-raspbian-armhf-part2.sh
+++ ci/travis-build-raspbian-armhf-part2.sh
@@ -10,7 +10,7 @@ DOCKER_CONTAINER_ID=$(sudo docker ps | grep raspbian | awk '{print $1}')
 
 echo $TRAVIS_BRANCH
 echo $OCPN_TARGET
-echo $BUILD_FLAGS
+
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     "export TRAVIS=$TRAVIS; export TRAVIS_BRANCH=$TRAVIS_BRANCH; export OCPN_TARGET=$OCPN_TARGET; export BUILD_FLAGS=$BUILD_FLAGS; rm -rf ci-source/build; mkdir ci-source/build; cd ci-source/build; cmake ..; make $BUILD_FLAGS; make package; chmod -R a+rw ../build;"
  
