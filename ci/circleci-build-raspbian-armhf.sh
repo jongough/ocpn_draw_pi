f1f12d21b0f0285c488d12db366570fa1b7af87d
jongough <jon.gough@eclipsesystems.com.au>
2021-01-27 09:00:52 +1100

    Update to match new image requirements [skip appveyor] [skip travis]

--- ci/circleci-build-raspbian-armhf.sh
+++ ci/circleci-build-raspbian-armhf.sh
@@ -23,27 +23,40 @@ docker run --privileged -d -ti -e "container=docker"  -v $(pwd):/ci-source:rw $D
 #DOCKER_CONTAINER_ID=$(docker ps | grep $BUILD_ENV | awk '{print $1}')
 DOCKER_CONTAINER_ID=$(docker ps | awk '/balenalib/ {print $1}')
 
-docker exec -ti $DOCKER_CONTAINER_ID sudo curl http://mirrordirector.raspbian.org/raspbian.public.key  | sudo apt-key add -
-docker exec -ti $DOCKER_CONTAINER_ID sudo curl http://archive.raspbian.org/raspbian.public.key  | sudo apt-key add -
+#docker exec -ti $DOCKER_CONTAINER_ID sudo curl http://mirrordirector.raspbian.org/raspbian.public.key  | sudo apt-key add -
+#docker exec -ti $DOCKER_CONTAINER_ID sudo curl http://archive.raspbian.org/raspbian.public.key  | sudo apt-key add -
 
-docker exec -ti $DOCKER_CONTAINER_ID sudo apt-get update
-docker exec -ti $DOCKER_CONTAINER_ID sudo apt-get -y install git cmake build-essential cmake gettext wx-common libgtk2.0-dev libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release
+#docker exec -ti $DOCKER_CONTAINER_ID sudo apt-get update
+#docker exec -ti $DOCKER_CONTAINER_ID sudo apt-get -y install git cmake build-essential cmake gettext wx-common libgtk2.0-dev libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release
 
 
 echo $OCPN_TARGET
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "export CIRCLECI=$CIRCLECI;
-    export CIRCLE_BRANCH=\"$CIRCLE_BRANCH\";
-    export CIRCLE_TAG=\"$CIRCLE_TAG\";
-    export CIRCLE_PROJECT_USERNAME=\"$CIRCLE_PROJECT_USERNAME\";
-    export CIRCLE_PROJECT_REPONAME=\"$CIRCLE_PROJECT_REPONAME\";
-    export GIT_REPOSITORY_SERVER=\"$GIT_REPOSITORY_SERVER\";
-    export OCPN_TARGET=$OCPN_TARGET;
-    export TRAVIS=$TRAVIS;
-    export TRAVIS_REPO_SLUG=\"$TRAVIS_REPO_SLUG\";
-    export TRAVIS_BRANCH=\"$TRAVIS_BRANCH\";
-    export TRAVIS_TAG=\"$TRAVIS_TAG\";
-    rm -rf ci-source/build; mkdir ci-source/build; cd ci-source/build; cmake ..; make $BUILD_FLAGS; make package; chmod -R a+rw ../build;"
+    -e "CIRCLECI=$CIRCLECI" \
+    -e "CIRCLE_BRANCH=$CIRCLE_BRANCH" \
+    -e "CIRCLE_TAG=$CIRCLE_TAG" \
+    -e "CIRCLE_PROJECT_USERNAME=$CIRCLE_PROJECT_USERNAME" \
+    -e "CIRCLE_PROJECT_REPONAME=$CIRCLE_PROJECT_REPONAME" \
+    -e "GIT_REPOSITORY_SERVER=$GIT_REPOSITORY_SERVER" \
+    -e "OCPN_TARGET=$OCPN_TARGET" \
+    -e "TRAVIS=$TRAVIS" \
+    -e "TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG" \
+    -e "TRAVIS_BRANCH=$TRAVIS_BRANCH" \
+    -e "TRAVIS_TAG=$TRAVIS_TAG" \
+    -v $(pwd):/ci-source:rw;
+
+# Run build script
+cat > build.sh << "EOF"
+sudo su
+curl http://mirrordirector.raspbian.org/raspbian.public.key  | apt-key add -
+curl http://archive.raspbian.org/raspbian.public.key  | apt-key add -
+sudo apt -q update
+sudo apt-get -y install --no-install-recommends \
+    git cmake build-essential cmake gettext wx-common libgtk2.0-dev libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release
+EOF
+
+docker exec -ti \
+    $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /ci-source/build.sh; rm -rf ci-source/build; mkdir ci-source/build; cd ci-source/build; cmake ..; make $BUILD_FLAGS; make package; chmod -R a+rw ../build;"
 
 echo "Stopping"
 docker ps -a
