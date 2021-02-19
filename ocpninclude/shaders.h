6c53779099f8f6c9f9c032f5afd018af83b571ea
dsr <bdbcat@yahoo.com>
2021-02-19 15:31:44 -0500

    Correct gl2 include file location for Android #2

--- ocpninclude/shaders.h
+++ ocpninclude/shaders.h
@@ -29,8 +29,8 @@
 #include "wx/wx.h"
 #endif //precompiled headers
 
-//#include <gl2.h>
-#include "/usr/include/GLES2/gl2.h"
+#include <gl2.h>
+//#include "/usr/include/GLES2/gl2.h"
 
     extern GLint color_tri_shader_program;
     extern GLint texture_2D_shader_program;
