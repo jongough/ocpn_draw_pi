0fc52a402022711e5a2c16be8c8278df09d17f8d
jongough <jon.gough@eclipsesystems.com.au>
2022-03-12 16:35:58 +1100

    Revert to android code for OpenGL

--- src/ODPoint.cpp
+++ src/ODPoint.cpp
@@ -616,8 +616,8 @@ void ODPoint::DrawGL( PlugIn_ViewPort &pivp )
         float ys = l_odp.y - hs/2;
         float u = (float)w / glw, v = (float)h / glh;
 
-//#ifdef USE_ANDROID_GLES2
-#if 0
+#ifdef USE_ANDROID_GLES2
+//#if 0
         float coords[8];
         float uv[8];
         // normal uv
@@ -642,8 +642,7 @@ void ODPoint::DrawGL( PlugIn_ViewPort &pivp )
         glChartCanvas::RenderSingleTexture(coords, uv, &vp, 0, 0, 0);
 
 #else
-
-        glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+        glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
 
         glColor3f(1, 1, 1);
 
