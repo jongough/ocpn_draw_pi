98281a818a23dc573535c96042c19c4e0e806fde
jongough <jon.gough@eclipsesystems.com.au>
2023-06-22 17:19:24 +1000

    Force debian for jammy

--- cmake/PluginSetup.cmake
+++ cmake/PluginSetup.cmake
@@ -120,7 +120,7 @@ elseif(UNIX)
         message(STATUS "${CMLOC}PKG_TARGET_WX_VER: ${PKG_TARGET_WX_VER}, WX_VER: $ENV{WX_VER}")
     elseif(("$ENV{BUILD_ENV}" STREQUAL "ubuntu" OR "$ENV{BUILD_ENV}" STREQUAL "UBUNTU") AND
             (("$ENV{OCPN_TARGET}" STREQUAL "jammy" OR "$ENV{OCPN_TARGET}" STREQUAL "JAMMY")))
-        set(PKG_TARGET "$ENV{BUILD_ENV}")
+        set(PKG_TARGET "debian")
         if( NOT "$ENV{WX_VER}" STREQUAL "" )
             set(PKG_TARGET_WX_VER "-wx$ENV{WX_VER}")
         endif()
