6cba42dcca465b2c9530ad7adaca5b2ba9aa9934
jongough <jon.gough@eclipsesystems.com.au>
2023-07-17 18:09:46 +1000

    Update to FE2 1.0.248.0

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -35,7 +35,7 @@ echo ${CIRCLE_JOB}
 EXTRA_LIBS=./ci/extras/${CIRCLE_JOB}_extra_libs.txt
 echo $EXTRA_LIBS
 if test -f "$EXTRA_LIBS"; then
-    echo "Found build-jammy_extra_libs.txt"
+    echo "Found circleci_build-jammy_extra_libs.txt"
     while read -r line; do
         sudo apt-get install $line
     done < "$EXTRA_LIBS"
