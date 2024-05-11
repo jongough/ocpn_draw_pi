519a541d35df5841568b83fdae0f0c0dd651347e
jongough <2803240+jongough@users.noreply.github.com>
2024-05-11 12:11:29 +1000

    Update to add POEDIT to path

--- msvc/win_deps.bat
+++ msvc/win_deps.bat
@@ -49,7 +49,7 @@ if errorlevel 1 (
 ::
 set "POEDIT_HOME=C:\Program Files (x86)\Poedit\Gettexttools"
 if not exist "%POEDIT_HOME%" choco install -y poedit
-::pathman add "%POEDIT_HOME%\bin" > nul
+pathman add "%POEDIT_HOME%\bin" > nul
 
 :: Update required python stuff
 ::
